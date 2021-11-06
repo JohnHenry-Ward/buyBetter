92f3909ab6163e31b1666bc4cf7d8183807ac792
JohnHenry <johnhenry514@gmail.com>
2021-11-06 13:41:23 -0700

    added listing of seller so we can direct them to their online store

--- server.js
+++ server.js
@@ -24,8 +24,10 @@ app.get('/getInfo', (req, res) => {
 
             let ASIN = $('#ASIN')['0']['attribs']['value']; //get the ASIN of the product
 
-            let UPC = ''; //get the UPC of the product (if it is listed)
-            let UPClocation = $('#detailBullets_feature_div').find('span').find('span').text();
+            //get the UPC of the product (if it is listed)
+            let UPC = ''; 
+            let details = $('#detailBullets_feature_div');
+            let UPClocation = details.find('span').find('span').text();
 
             for(let i = 0; i < UPClocation.length; i++) {
                 if(UPClocation.substring(i, i+3) == 'UPC'){
@@ -33,29 +35,44 @@ app.get('/getInfo', (req, res) => {
                 }
             }
 
-            if(UPC == ''){
+            if(UPC == '') {
                 UPC = 'UPC is not listed';
             }
 
-            let departments = ''; //get the depeartments the product is categorized as
+            //get the depeartments the product is categorized as
+            let departments = ''; 
             let deptLocation = $('#wayfinding-breadcrumbs_feature_div');
             departments = deptLocation.text();
             deptArr = departments.split('›');
 
-            res.send(`<h2>Product: ${product} <h2><br>
-                      <h2>ASIN: ${ASIN} <h2><br>
-                      <h2>UPC: ${UPC} <h2><br>
-                      <h2>Departments: ${deptArr} <h2><br>
-                      ${backBtn}`); //Display all the info
+            //get the name of the seller of this product
+            let soldBy = $('#productOverview_feature_div').children().find('tr:contains("Brand")').text();
+            soldBy = soldBy.replace('Brand', '').trim(); 
+            if (soldBy == '') {
+                soldBy = details.children().find('li:contains("Manufacturer")').last().text();
+                soldBy = soldBy.replace('Manufacturer', '').replace(':', '').replace(/(\r\n|\n|\r)/gm, '').trim();
+            }
+
+            if (soldBy == '') {
+                soldBy = 'Seller not listed';
+            } else {
+                soldBy += ' (check to see if they have an online store and buy directly from them!)';
+            }
+
+            //Display all the info
+            res.send(`<h2>Product: ${product} </h2><br>
+                      <h2>Sold By: ${soldBy} </h2><br>
+                      <h2>ASIN: ${ASIN} </h2><br>
+                      <h2>UPC: ${UPC} </h2><br>
+                      <h2>Departments: ${deptArr} </h2><br>
+                      ${backBtn}`); 
 
         })
         .catch(error => {
-            console.log(error.code);
-
-            res.send(`Error: ${error.code}
+            console.log(`Error: ${error}`);
+            res.send(`Error: ${error}, Code: ${error.code} <br>
                       Please check that the URL you submitted is a corrct Amazon.com product URL: ${URL}
                       ${backBtn}`);
-                     
         })
 })
 
