0597a9fba6f0d06ee34a6e36f258c004e32fe9f5
JohnHenry <johnhenry514@gmail.com>
2021-11-08 13:06:20 -0800

    added ability to get website URL's for listed stores

--- getPlaces/js/places.js
+++ getPlaces/js/places.js
@@ -10,15 +10,37 @@ function getQuery() {
 }
 
 function initMap(query) {
+  if (query == undefined || query == '') {
+    document.getElementById('temp').innerHTML = "<h1>Search to display the map</h1>";
+    return;
+  }
+
+  document.getElementById('temp').style.display = "none";
+  document.getElementById('temp').style.visibility = "hidden";
+
+  console.log(`Query: ${query}`);
   const HERE = new google.maps.LatLng(48.724430, -122.487360); //currently hardcoded latitude and longitude
+  const userPosition = {lat: 48.724439, lng: -122.487360};
   infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: HERE,
-    zoom: 15
+  //initalize the map
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: userPosition,
+    zoom: 13,
   });
+  const marker = new google.maps.Marker({
+    map: map,
+    position: userPosition,
+    label: 'ME',
+  });
+  //add event listener over users marker
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent('Your Location');
+    infowindow.open(marker.get("map"), marker);
+  });
+  //create the request
   var request = {
       location: HERE ,
-      radius: '10000',
+      radius: '10000', //in meters
       keyword: query,
       type: ['store']
     };
@@ -28,22 +50,44 @@ function initMap(query) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       document.getElementById('output').innerHTML = '';
       for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
+        createMarker(results[i], map);
         document.getElementById('output').innerHTML += results[i].name + '<br>';
+        let placeID = results[i].place_id;
+        // storeURLrequest(placeID, map);
       }
       document.getElementById('map').style.display = 'block';
-      map.setCenter(results[0].geometry.location);
+      document.getElementById('map').style.visibility = 'visible';
+      document.getElementById('output').style.display = 'block';
+      document.getElementById('output').style.visibility = 'visible';
+      // map.setCenter(results[0].geometry.location);
     }
   });
 }
 
-function createMarker(place) {
+function createMarker(place, map) {
   const marker = new google.maps.Marker({
-    map,
+    map: map,
     position: place.geometry.location
   });
   google.maps.event.addListener(marker, "click", () => {
     infowindow.setContent(place.name);
-    infowindow.open(map);
+    infowindow.open(marker.get("map"), marker);
   });
+}
+
+function storeURLrequest(id, map) {
+  console.log('test');
+  let req = {
+    placeId: id,
+    fields: ['website'],
+  }
+
+  service = new google.maps.places.PlacesService(map);
+  service.getDetails(req, (response, status) => {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      console.log(response);
+    } else {
+      console.log(status);
+    }
+  })
 }
