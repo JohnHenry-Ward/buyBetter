0bb2728e07b0625430930bccd87de178088d5282
Alex Wilson <wilso292@wwu.edu>
2021-11-09 08:26:12 -0800

    updated this repo so I can see the map, and the API works 100%

---
+++ getPlaces/src/getLocation.js
@@ -0,0 +1,57 @@
+// This is to get the latitude and longitude of the user
+
+
+// let button = document.getElementById("get-location");
+
+// button.addEventListener("click", function() {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     let lat = position.coords.latitude;
+//     let long = position.coords.longitude;
+
+//     console.log(`Lat: ${lat}, Long: ${long}`);
+//   });
+// });
+
+let map;
+let service;
+let infowindow;
+
+function initMap() {
+  const sydney = new google.maps.LatLng(-33.867, 151.195);
+
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: sydney,
+    zoom: 15,
+  });
+
+  const request = {
+    query: "Museum of Contemporary Art Australia",
+    fields: ["name", "geometry"],
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
